cbe5d6ed1777981cc2e091a2bd63d37ee90740cd
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-08-03 19:08:23 +0200

    feat: minify output

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "rollup"
 import typescript from "rollup-plugin-typescript2"
 import shebang from "rollup-plugin-preserve-shebang"
+import terser from "@rollup/plugin-terser"
 
 export default defineConfig({
   input: "src/index.ts",
@@ -8,5 +9,5 @@ export default defineConfig({
     file: "dist/index.mjs",
     format: "module",
   },
-  plugins: [typescript(), shebang()],
+  plugins: [typescript(), shebang(), terser()],
 })
